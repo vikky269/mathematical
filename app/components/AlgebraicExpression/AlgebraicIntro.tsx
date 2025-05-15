05668efec13ba41e4d9932faf89e5bb527b627b0
vikky269 <viktorefi2001@gmail.com>
2025-05-15 15:55:54 +0100

    aaa

--- app/components/AlgebraicExpression/AlgebraicIntro.tsx
+++ app/components/AlgebraicExpression/AlgebraicIntro.tsx
@@ -4,14 +4,15 @@ import { motion } from 'framer-motion';
 const Introduction = () => (
   <section className="mt-3">
     <h2 className="md:text-3xl text-2xl font-bold mb-3">Algebraic Expressions</h2>
-    <p className='text-gray-800 mb-4 mt-6'>
+    {/* <p className='text-gray-800 mb-4 mt-6'>
       Algebraic expressions are mathematical statements formed by combining variables, constants, and
-      arithmetic operations. For example, let us assume that James and Natalie were playing with matchsticks and thought of forming number patterns using them. 
+      arithmetic operations. For example, let us assume that James and Natalie were playing with
+      matchsticks and thought of forming number patterns using them. 
       James took four matchsticks and formed the number 4. Natalie added three more matchsticks to form a pattern with two 4's. 
       They realized that they can keep on adding 3 matchsticks in each round to make one extra "four". 
       From this, they concluded that they need 4 + 3(n-1) sticks, in general, to make a pattern with n number of 4's. 
       Here, 4 + 3(n-1) is called an algebraic expression.
-    </p>
+    </p> */}
   </section>
 )
 
